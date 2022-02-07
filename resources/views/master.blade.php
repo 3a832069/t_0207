2c7ac2a2c4525f1b053957f91bd2f5e85b83fb5d
Len-En Chiu <linda34264622@gmail.com>
2022-02-07 15:04:56 +0800

    修復輪播bug

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,7 +9,8 @@
     <title>Carousel Template · Bootstrap v5.1</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/carousel/">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
 
     <!-- Bootstrap core CSS -->
